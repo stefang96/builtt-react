7a985d1692bbf1d339df060301eacb33ff9a1769
Stefan Grujicic <stefan.grujicic@waltercode.com>
2023-11-24 23:59:45 +0100

    add title per page

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -7,12 +7,18 @@ import TotalAmount from "./TotalAmount";
  */
 const Cart = () => {
 	return (
-		<div className="px-28 grid grid-cols-3 gap-14">
-			<div className="col-span-2">
-				<CartInformation />
+		<div className="px-28">
+			<div className="mb-7">
+				<span className="text-xl font-bold">Tvoja korpa</span>
 			</div>
-			<div>
-				<TotalAmount />
+
+			<div className="grid grid-cols-1 gap-14  lg:grid-cols-3">
+				<div className="col-span-2">
+					<CartInformation />
+				</div>
+				<div>
+					<TotalAmount />
+				</div>
 			</div>
 		</div>
 	);
--- src/components/products/Products.js
+++ src/components/products/Products.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { connect } from "react-redux";
 import Product from "./single-view/Product";
 
@@ -10,15 +10,27 @@ import Product from "./single-view/Product";
 const Products = (props) => {
 	const { products } = props;
 
+	const numberOfProducts = useMemo(() => {
+		return products?.length;
+	}, [products]);
+
 	return (
-		<div className="px-28 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
-			{products.map((product) => {
-				return (
-					<div key={product.id}>
-						<Product product={product} />
-					</div>
-				);
-			})}
+		<div className="px-28">
+			<div className="mb-7">
+				<span className="text-xl font-bold">Svi proizvodi</span>
+				<span className="text-base  opacity-50 ml-2">
+					{numberOfProducts} proizvoda
+				</span>
+			</div>
+			<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">
+				{products.map((product) => {
+					return (
+						<div key={product.id}>
+							<Product product={product} />
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 };
